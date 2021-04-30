ff8c649f201f4627acce22c4d4f9261dd5af2173
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-30 19:52:34 -0400

    feat: global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import styled from 'styled-components';
-import Header from './components/header';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import styled from "styled-components";
+import Header from "./components/header";
+import NewsFeed from "./components/newsFeed";
+import Stats from "./components/stats";
 
 function App() {
   return (
@@ -10,11 +12,31 @@ function App() {
       <HeaderContainer>
         <Header />
       </HeaderContainer>
+      <AppBody>
+        <AppContainer>
+          <NewsFeed />
+          <Stats />
+        </AppContainer>
+      </AppBody>
     </div>
   );
 }
 
 export default App;
 
+const HeaderContainer = styled.div``;
 
-const HeaderContainer = styled.div``
+const AppBody = styled.div`
+  width: 100%;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+`;
+
+const AppContainer = styled.div`
+  width: 1024px;
+  height: 100%;
+  display: flex;
+  padding-left: 48px;
+  padding-right: 48px;
+`;
